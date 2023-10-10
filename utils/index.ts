5b74bedf0b3e52fdb3541d0ebf712e5f16a5f3f4
steve3ph <144018010+steve3ph@users.noreply.github.com>
2023-10-10 13:27:15 +0530

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -46,7 +46,7 @@ export async function fetchCars(filters: FilterProps) {
 
   // Set the required headers for the API request
   const headers: HeadersInit = {
-    "X-RapidAPI-Key": '54370c02aamsh120f85b201f85fcp1139d9jsn843cfbfa0e79' || "",
+    "X-RapidAPI-Key": '54370c02aamsh120f85b201f85fcp1139d9jsn843cfbfa0e79' || "", //new key of yours
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
